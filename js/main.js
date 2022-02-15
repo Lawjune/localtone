701ca0c1095df9f2dbe9e697b7622f1d723a23d7
Lawjune <lawjune@163.com>
2022-02-15 15:41:42 +0800

    Added playing music and queue tones.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 let delay_ms = 200;
-const correction_factor = 40;
 
 const btn_set = document.querySelector(".set-delay");
 btn_set.addEventListener("click", () => {
@@ -11,29 +10,54 @@ btn_set.addEventListener("click", () => {
 const btn_1 = document.querySelector(".btn-1");
 // const audio_1 = new Audio("./res/button-rollover.mp3");
 const audio_1 = document.getElementById("audio-1");
+const play_audio_1 = () => {
+  console.log("play_audio_1");
+  audio_1.play();
+};
 
 const btn_2 = document.querySelector(".btn-2");
 // const audio_2 = new Audio("./res/button-switch.mp3");
 const audio_2 = document.getElementById("audio-2");
+const play_audio_2 = () => {
+  console.log("play_audio_2");
+  audio_2.play();
+};
 
 const btn_3 = document.querySelector(".btn-3");
 // const audio_3 = new Audio("./res/button-check.mp3");
 const audio_3 = document.getElementById("audio-3");
+const play_audio_3 = () => {
+  console.log("play_audio_3");
+  audio_3.play();
+};
 
 btn_1.addEventListener("click", () => {
-  setTimeout(() => {
-    audio_1.play();
-  }, delay_ms);
+  setTimeout(() => play_audio_1(), delay_ms);
 });
 
 btn_2.addEventListener("click", () => {
-  setTimeout(() => {
-    audio_2.play();
-  }, delay_ms);
+  setTimeout(() => play_audio_2(), delay_ms);
 });
 
 btn_3.addEventListener("click", () => {
-  setTimeout(() => {
-    audio_3.play();
-  }, delay_ms);
+  setTimeout(() => play_audio_3(), delay_ms);
+});
+
+const btn_music_play = document.querySelector(".btn-music-play");
+const btn_music_pause = document.querySelector(".btn-music-pause");
+const btn_music_stop = document.querySelector(".btn-music-stop");
+const music = document.getElementById("music");
+
+btn_music_play.addEventListener("click", () => music.play());
+btn_music_pause.addEventListener("click", () => music.pause());
+btn_music_stop.addEventListener("click", () => {
+  music.pause();
+  music.currentTime = 0;
+});
+
+const btn_interval = document.querySelector(".btn-interval");
+btn_interval.addEventListener("click", () => {
+  const interval = document.getElementById("interval-ms").value;
+  play_audio_2();
+  setTimeout(() => play_audio_3(), interval);
 });
